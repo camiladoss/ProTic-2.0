d52c5f4e97d7fdb7657e31724d86df3beabfe826
Carlos <carlosesp10@gmail.com>
2021-11-17 20:29:41 -0500

    Se arregla el input fecha

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ import PrivateLayout from 'Layouts/PrivateLayout'
 import GestionProyectos from "pages/GestionProyectos";
 import CrearProyectos from "pages/CrearProyectos";
 import CrearAvances from "pages/CrearAvances";
+import GestionAvances from "pages/GestionAvances";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes> 
+      <Routes>
         <Route  path="/" element={<Layout/>}>
           <Route path="/" element={<Index/>} />
         </Route>
@@ -23,9 +24,9 @@ function App() {
           <Route path="/GestionProyectos" element={<GestionProyectos/>} />
           <Route path="/CrearProyectos" element={<CrearProyectos/>} />
           <Route path="/CrearAvances" element={<CrearAvances/>} />
+          <Route path="/GestionAvances" element={<GestionAvances/>} />
         </Route>
-
-      </Routes>     
+      </Routes>
   </BrowserRouter>
   );
 }

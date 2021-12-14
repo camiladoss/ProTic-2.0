98dc96c99645f045c5214f173833b54d78bf43a6
Carlos <carlosesp10@gmail.com>
2021-12-13 21:57:31 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PostRequest />
   </React.StrictMode>,
   document.getElementById('root')
 );

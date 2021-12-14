ef4add49c7e86b543f37e8a918ace3e114530fab
Carlos <carlosesp10@gmail.com>
2021-12-14 18:18:14 -0500

    Se corrigen algunos errores

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from 'components/Navbar'
 import {Outlet} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,7 +6,6 @@ import 'react-toastify/dist/ReactToastify.css';
 const AuthLayout = () => {
     return (
         <div>
-            {/* <Navbar /> */}
             <Outlet className="w-full"/>
             <ToastContainer position="bottom-center" />
         </div>
